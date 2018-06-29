eac0b4ed0a2425df31150c9eb157fa9ee4a9eb83
Cascode6 <caseylfaist@gmail.com>
2018-06-29 12:11:39 -0400

    add interactive prompt response and test

--- test/determinegoal_test.sh
+++ test/determinegoal_test.sh
@@ -1,39 +1,59 @@
 #!/bin/bash
 
 . ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
+mkdir ${BUILDPACK_HOME}/determine
 
 testDetermineBlankGoal() {
-    
-    echo "Calling determine-goal with ''..."
-    capture ${BUILDPACK_HOME}/bin/determine-goal <<""
-    
-    assertContains "THIS IS PYTHON 2!" "`cat ${STD_OUT}`"
-    assertContains "Unfortunately," "`cat ${STD_OUT}`"
-    assertContains "Your options are" "`cat ${STD_OUT}`"
+    touch ${BUILDPACK_HOME}/determine/goal.txt
+    echo '\n\n\n' > ${BUILDPACK_HOME}/determine/goal.txt
 
+    capture ${BUILDPACK_HOME}/bin/determine-goal
+
+    assertContains "THIS IS PYTHON 2!" "`cat ${STD_OUT}`"
+    assertContains "got it" "`cat ${STD_OUT}`"
     assertContains "Goodbye" "`cat ${STD_OUT}`"
+    assertNotContains "dangit" "`cat ${STD_OUT}`"
+    assertEquals 0 ${rtrn}
 }
 
 testDetermineUpgradeGoal() {
-    
-    echo "Calling determine-goal with 'u'..."
-    capture ${BUILDPACK_HOME}/bin/determine-goal <<"u"
-    assertContains "THIS IS PYTHON 2!" "`cat ${STD_OUT}`"
-    assertContains "Unfortunately," "`cat ${STD_OUT}`"
-    assertContains "Your options are" "`cat ${STD_OUT}`"
-    assertContains "Upgrade!!!!!!!!!" "`cat ${STD_OUT}`"
+    touch ${BUILDPACK_HOME}/determine/goal.txt
+    echo $'u\n\n' > ${BUILDPACK_HOME}/determine/goal.txt
 
+    capture ${BUILDPACK_HOME}/bin/determine-goal
+
+    assertContains "THIS IS PYTHON 2!" "`cat ${STD_OUT}`"
+    assertContains "got it" "`cat ${STD_OUT}`"
+    assertContains "Upgrade" "`cat ${STD_OUT}`"
+    
     assertNotContains "Goodbye" "`cat ${STD_OUT}`"
+    assertNotContains "dangit" "`cat ${STD_OUT}`"
+    assertEquals 0 ${rtrn}
 }
 
 testDetermineDowngradeGoal() {
-    
-    echo "Calling determine-goal with 'd'..."
-    ${BUILDPACK_HOME}/bin/determine-goal <<"d"
-    assertContains "THIS IS PYTHON 2!" "`cat ${STD_OUT}`"
-    assertContains "Unfortunately," "`cat ${STD_OUT}`"
-    assertContains "Your options are" "`cat ${STD_OUT}`"
-    assertContains "Downgrade!!!!!!!!!" "`cat ${STD_OUT}`"
+    touch ${BUILDPACK_HOME}/determine/goal.txt
+    echo $'d\n\n' > ${BUILDPACK_HOME}/determine/goal.txt
 
-    assertNotContains "Goodbye" "`cat ${STD_OUT}`"   
+    capture ${BUILDPACK_HOME}/bin/determine-goal
+
+    assertContains "THIS IS PYTHON 2!" "`cat ${STD_OUT}`"
+    assertContains "got it" "`cat ${STD_OUT}`"
+    assertContains "Downgrade" "`cat ${STD_OUT}`"
+    assertNotContains "dangit" "`cat ${STD_OUT}`"
+    assertNotContains "Goodbye" "`cat ${STD_OUT}`"
+    assertEquals 0 ${rtrn}
+}
+
+testDetermineInvalidResponse() {
+    touch ${BUILDPACK_HOME}/determine/goal.txt
+    echo $'y\n\n' > ${BUILDPACK_HOME}/determine/goal.txt
+
+    capture ${BUILDPACK_HOME}/bin/determine-goal
+
+    assertContains "THIS IS PYTHON 2!" "`cat ${STD_OUT}`"
+    assertContains "got it" "`cat ${STD_OUT}`"
+    assertContains "Goodbye" "`cat ${STD_OUT}`"
+    assertNotContains "dangit" "`cat ${STD_OUT}`"
+    assertEquals 0 ${rtrn}
 }
